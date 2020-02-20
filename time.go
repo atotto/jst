3a4b3e3aae5a32ca12f1ebf1873ee574d9188cd5
Ato Araki <ato.araki@gmail.com>
2020-02-20 23:06:54 +0900

    add JST

--- time.go
+++ time.go
@@ -3,9 +3,13 @@ package jst
 import "time"
 
 var (
-	JST = time.FixedZone("JST", 9*60*60)
+	Location = time.FixedZone("JST", 9*60*60)
 )
 
 func Now() time.Time {
-	return time.Now().In(JST)
+	return time.Now().In(Location)
+}
+
+func JST(t time.Time) time.Time {
+	return t.In(Location)
 }
